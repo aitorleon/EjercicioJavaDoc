8f0ff746a004be873519effc99fd62f402f58cb3
ikaslea <ikaslea@hz110806.zornotza.local>
2023-01-31 12:50:09 +0100

    Ejercicio Logs

--- Calculadora/src/principal/Calculadora.java
+++ Calculadora/src/principal/Calculadora.java
@@ -1,13 +1,48 @@
 package principal;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import logs.LogConsolaMasFichero;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 import menu.Menu;
 import operaciones.Operaciones;
 
 public class Calculadora{
-    public static void main(String[] args) {   
+	private static final Logger LOGGER = Logger.getLogger(Calculadora.class.getName());
+    public static void main(String[] args) { 
         int resultado = 0;
         String operacion = "";
         int[] operandos = new int [2];
-        
+        Handler consoleHandler = null;
+	    Handler fileHandler  = null;
+	    try{
+            //Crear consoleHandler y fileHandler
+            consoleHandler = new ConsoleHandler();
+            fileHandler  = new FileHandler("./operaciones.log");
+            
+             
+            //Asignar handlers al objeto LOGGER
+            LOGGER.setUseParentHandlers(false);
+            LOGGER.addHandler(consoleHandler);
+            LOGGER.addHandler(fileHandler);
+             
+            //Establecer niveles a handlers y LOGGER
+            consoleHandler.setLevel(Level.WARNING);
+            fileHandler.setLevel(Level.ALL);
+            LOGGER.setLevel(Level.ALL);
+			
+        }catch(IOException exception){
+            LOGGER.log(Level.WARNING, "Ocurrió un error en FileHandler.", exception);
+        }
         Menu menu = new Menu();
         Operaciones operaciones = new Operaciones();
         
@@ -28,8 +63,9 @@ public class Calculadora{
             	try {
                 resultado = operaciones.dividir(operandos[0], operandos[1]);
                 System.out.println ("Resultado: " + resultado);
+                LOGGER.log(Level.FINE,"El resultado ha sido completado--->" + resultado);
             	}catch (ArithmeticException excepcion) {
-            		System.out.println("ERROR" + excepcion.getMessage());
+            		LOGGER.log(Level.WARNING, "Division entre 0", excepcion);
             	}
             } else if (operacion.equalsIgnoreCase("%")){
                 resultado = operaciones.resto(operandos[0], operandos[1]);
@@ -38,5 +74,7 @@ public class Calculadora{
                 System.out.println ("Operaci�n no v�lida");
             }
         }   while (menu.repetir());
+        consoleHandler.close();
+        fileHandler.close();
     }
 }
